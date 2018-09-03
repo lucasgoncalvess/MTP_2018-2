46e3ee43e4ac98b399ba8bb95e86831c8eff2d92
lucasgoncalvess <42481259+lucasgoncalvess@users.noreply.github.com>
2018-09-03 13:37:13 -0300

    Add files via upload
    Trabalho 1

---
+++ P0.c
@@ -0,0 +1,18 @@
+/* P0.c */
+/* Lucas Gonçalves e Silva */
+/* 11811EAU016 */
+
+#include<stdio.h>
+int main(int argc, char ** argv){
+int i;
+if(argc > 1){
+if(argc > 2)
+for(i = 0;i < argv[2][0]-'0'; i++)
+printf("Valeu, %s\n",argv[1]);
+else
+printf("Saudacoes, %s\n",argv[1]);
+}
+else
+printf("Ola!\n");
+return 0;
+}
