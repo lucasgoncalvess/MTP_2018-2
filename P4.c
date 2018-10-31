e7c0dd6406896802bec04734d56a8920fd2dbc9b
Lucas Gonçalves e Silva <42481259+lucasgoncalvess@users.noreply.github.com>
2018-10-31 09:08:45 -0300

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,27 @@
+//Lucas Gonçalves e Silva 
+//11811EAU016
+
+#include <stdio.h>
+
+int ackermann(int, int);
+int main ()
+{
+	int m, n;
+	printf("Digite os valores de m e n, respectivamente:\n");
+	scanf("%d", &m);
+	getchar();
+	scanf("%d", &n);
+	getchar();
+	printf("%d", ackermann(m, n));
+	return 0;
+}
+
+int ackermann(int m, int n)
+{
+	if (m == 0)
+	return n+1;
+	else if (m>0 && n==0)
+	return ackermann(m-1, 1);
+	else if (m>0 && n>0)
+	return ackermann(m-1, ackermann(m, n-1));
+}
