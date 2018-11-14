668e7866c99e324fe936fce7585d5296a73b09d8
Lucas Gonçalves e Silva <42481259+lucasgoncalvess@users.noreply.github.com>
2018-11-14 19:21:39 -0200

    Update P6.c

--- P6.c
+++ P6.c
@@ -1,30 +1,43 @@
-//Lucas Gonçalves e Silva 
+//Lucas GonÃ§alves e Silva 
 //11811EAU016
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <stdio.h>
 
-#define r 1
-#define pi 3.14159265359
+typedef
+	struct p {
+		float x;
+		float y;
+	}
+Ponto;
 
-typedef struct {
-  float x, y;
-}Ponto;
+Ponto * geraPontos(int N){
+	Ponto * pontos = (Ponto *) calloc(N,sizeof(Ponto));
+	int i;
+	for(i=0; i<N; i++){
+		pontos[i].x = cos(i*2.0*M_PI/(N-1));
+		pontos[i].y = sin(i*2.0*M_PI/(N-1));
+	}
+	return pontos;
+}
 
-int main(){
-   int N, i; 
-   Ponto *xy;
-   scanf("%i", &N);getchar();
-   xy= (Ponto *) malloc(N*sizeof(Ponto));
-   printf("%i retorna \" ",N);
+void mostraPontos(Ponto * pi, Ponto * pf){
+	if(pi < pf){
+		printf("(%.3f, %.3f) ", pi->x, pi->y);
+		mostraPontos(pi+1, pf);
+	}
+}
 
-   for(i=0; i<N; i++){
-     xy->x= r*cos(i*2*pi/(N));
-     xy->y= r*sin(i*2*pi/N);
-     printf("(%.3f, %.3f) ", xy->x, xy->y);
-   }
-   printf("\" ");
-   free(xy);
-   return 0;
+int main()
+{
+	unsigned int N;
+	Ponto * pontos;
+	printf("Digite o numero de pontos: ");
+	scanf("%d",&N);
+	getchar();
+	pontos = geraPontos(N);
+	mostraPontos(pontos, pontos+N);
+	free(pontos);
+	return 0;
 }
